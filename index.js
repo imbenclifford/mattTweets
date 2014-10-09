9ae4580a61b835fa8dc6e288f00f53a2b092cafd
maryams <maryamsarang2014@gmail.com>
2014-10-09 11:53:11 +0000

    thirdttest

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ twitpic();
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-	response.header('Access-Control-Allow-Origin', '*');
   var arrayJSON = fs.readFileSync('array.json', 'utf8', function (err, data) {
     if (err) throw err;
     return(data);
