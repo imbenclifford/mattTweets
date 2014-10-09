8dfcbc4faeba87fc5592bc686e48f78de417d916
maryams <maryamsarang2014@gmail.com>
2014-10-09 11:05:58 +0000

    firstcommit

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var http = require('http');
 
 
-
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
 
 var port = process.env.PORT || 3000;
-	app.listen(port);
+	server.listen(port);
 
