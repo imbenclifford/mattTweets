0919d25bd33edfc7f71dcce8f49e9fadd4889b72
imbenclifford <imbenclifford@gmail.com>
2014-10-10 11:32:08 +0000

    making-credentials-secure

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var twitpic = require('./twitpic')
-var fs = require('fs')
+var twitpic = require('./twitpic');
+var fs = require('fs');
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {
     "Content-Type": "text/plain",
     "Access-Control-Allow-Origin": "*"
-                          });
+    });
   twitpic();
 
   fs.readFile('array.json', 'utf8', function (err, data) {
@@ -14,8 +14,6 @@ var server = http.createServer(function (request, response) {
     response.end(data);
   });
 });
-
-  
 	server.listen(process.env.PORT || 3000);
 
 
--- twitpic.js
+++ twitpic.js
@@ -1,11 +1,12 @@
 var Twit = require('twit');
-var fs = require('fs')
+var fs = require('fs');
+var credentials = require('./credentials');
 
 var T = new Twit({
-   consumer_key: 'MS7aeU2RotLM7ikiIT6QlVi6K',
-   consumer_secret: 'gkYIk08uufXLXTyMEfmKB0sX0A4mDe4BRcPNCX6vYIemVmcBjb', 
-   access_token: '2813311471-tuLTn0j6mkTDK74H4BcRg3qh0xvEBIdGCpz3Zxc',
-   access_token_secret: 'psxk2F9aBYhBwj479vwGQTTYGP1Any7ChrHJFOFzfJ2oq'
+   consumer_key: credentials.consumer_key,
+   consumer_secret: credentials.consumer_secret, 
+   access_token: credentials.access_token,
+   access_token_secret: credentials.access_token_secret
 });
 
 module.exports = function () {
