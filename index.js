eae9fd8885059b6c1a233f384d05bb5369d91998
maryams <maryamsarang2014@gmail.com>
2014-10-10 09:24:46 +0000

    test3

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-var http = require('http');
 var twitpic = require('./twitpic')
 var fs = require('fs')
-
-
-twitpic();
+var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {
-		"Content-Type": "text/plain",
-		"Access-Control-Allow-Origin": "*"
-	});
-	
-  var arrayJSON = fs.readFileSync('array.json', 'utf8', function (err, data) {
+    "Content-Type": "text/plain",
+    "Access-Control-Allow-Origin": "*"
+                          });
+  twitpic();
+
+  fs.readFile('array.json', 'utf8', function (err, data) {
     if (err) throw err;
-    return(data);
+    response.end(data);
   });
-  response.end(arrayJSON);
-  
 });
+
   
 	server.listen(process.env.PORT || 3000);
 
