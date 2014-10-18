7a2e532e1faf4e564966742b260eb9abf8fd3bd3
imbenclifford <imbenclifford@gmail.com>
2014-10-18 09:07:35 +0000

    remove-gitignore-for-heroku

---
+++ credentials.js
@@ -0,0 +1,8 @@
+var credentials = {
+   consumer_key: 'MS7aeU2RotLM7ikiIT6QlVi6K',
+   consumer_secret: 'gkYIk08uufXLXTyMEfmKB0sX0A4mDe4BRcPNCX6vYIemVmcBjb', 
+   access_token: '2813311471-tuLTn0j6mkTDK74H4BcRg3qh0xvEBIdGCpz3Zxc',
+   access_token_secret: 'psxk2F9aBYhBwj479vwGQTTYGP1Any7ChrHJFOFzfJ2oq'
+  };
+
+module.exports = credentials
