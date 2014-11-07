930db0bc38511cb35aa11fe160982a4f6936e64a
imbenclifford <imbenclifford@gmail.com>
2014-11-07 15:51:15 +0000

    cred-with-nat

--- credentials.js
+++ credentials.js
@@ -2,7 +2,8 @@ var credentials = {
    consumer_key: 'MS7aeU2RotLM7ikiIT6QlVi6K',
    consumer_secret: 'gkYIk08uufXLXTyMEfmKB0sX0A4mDe4BRcPNCX6vYIemVmcBjb', 
    access_token: '2813311471-tuLTn0j6mkTDK74H4BcRg3qh0xvEBIdGCpz3Zxc',
-   access_token_secret: 'psxk2F9aBYhBwj479vwGQTTYGP1Any7ChrHJFOFzfJ2oq'
+   access_token_secret: 'psxk2F9aBYhBwj479vwGQTTYGP1Any7ChrHJFOFzfJ2oq',
+   db: 'mongodb://BC:BCRox@linus.mongohq.com:10029/coderunnertests'
   };
 
 module.exports = credentials
